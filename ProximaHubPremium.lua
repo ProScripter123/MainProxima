e13c8ffedc266452176c09b8996cc2796c98ccae
ProScripter123 <121627858+ProScripter123@users.noreply.github.com>
2025-02-05 15:03:45 +0700

    Update ProximaHubPremium.lua

--- ProximaHubPremium.lua
+++ ProximaHubPremium.lua
@@ -1,4 +1,4 @@
-if game.PlaceId = 6558526079
+if game.PlaceId = 6558526079 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ProScripter123/P-X-Z-AzBzx--305--205/refs/heads/main/AWTD.lua"))()
 else
 print("Game Not Found!")
