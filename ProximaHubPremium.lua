fecf689002cab48cba45f56a7162335e08ce7bec
ProScripter123 <121627858+ProScripter123@users.noreply.github.com>
2025-02-14 23:07:22 +0700

    Update ProximaHubPremium.lua

--- ProximaHubPremium.lua
+++ ProximaHubPremium.lua
@@ -1,5 +1,7 @@
 if game.PlaceId == 6558526079 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ProScripter123/P-X-Z-AzBzx--305--205/refs/heads/main/AWTD.lua"))()
+elseif game.PlaceId == 15560168390 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ProScripter123/P-X-Z-AzBzx--305--205/refs/heads/main/TVTD.lua"))()
 else
 print("Game Not Found!")
 end
